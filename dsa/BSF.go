5dd317d6bf4f40cea4cbc90fd1707cd9b15892f7
Guilherme Rodrigues <guilher.c.rodrigues@gmail.com>
2024-09-25 01:05:55 -0300

    feat. rewrote bfs for better understanding.

--- dsa/BSF.go
+++ dsa/BSF.go
@@ -1,10 +1,11 @@
 package dsa
 
-func (g *Graph) BFS(start int) (order []int) {
-	seen := make(map[int]bool)
+func (g *Graph) BFS(root int) (order []int) {
+	seen := map[int]bool{}
+
 	q := NewQueue()
-	q.Enqueue(start)
-	seen[start] = true
+	q.Enqueue(root)
+	seen[root] = true
 
 	for !q.IsEmpty() {
 		v, _ := q.Dequeue()
@@ -19,3 +20,23 @@ func (g *Graph) BFS(start int) (order []int) {
 
 	return order
 }
+
+// func (g *Graph) BFS(start int) (order []int) {
+// 	seen := make(map[int]bool)
+// 	q := NewQueue()
+// 	q.Enqueue(start)
+// 	seen[start] = true
+//
+// 	for !q.IsEmpty() {
+// 		v, _ := q.Dequeue()
+// 		order = append(order, v)
+// 		for _, val := range g.Edges[v] {
+// 			if !seen[val] {
+// 				q.Enqueue(val)
+// 				seen[val] = true
+// 			}
+// 		}
+// 	}
+//
+// 	return order
+// }
